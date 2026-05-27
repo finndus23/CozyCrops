7fe1f360558d944246ac72f00cffc2b114103188
Marcmarcdich <marc.vaassen@alumni.fh-aachen.de>
2026-05-27 14:08:00 +0200

    Marktplatz
    Design + npcs

--- Assets/Scripts/Sounds/MusicManager.cs
+++ Assets/Scripts/Sounds/MusicManager.cs
@@ -1,22 +1,50 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MusicManager : MonoBehaviour
 {
     private static MusicManager instance;
+    private AudioSource audioSource;
+
+    [SerializeField] private string targetScene = "MainMenu";
 
     private void Awake()
     {
-        // Prüfen ob schon ein MusicManager existiert
+        // Prüfen ob schon ein Manager existiert
         if (instance != null && instance != this)
         {
             Destroy(gameObject);
             return;
         }
 
-        // Diesen Manager speichern
         instance = this;
 
-        // Objekt beim Szenenwechsel behalten
+        // Objekt zwischen Szenen behalten
         DontDestroyOnLoad(gameObject);
+
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // Nur in bestimmter Szene Musik abspielen
+        if (scene.name == targetScene)
+        {
+            audioSource.Play();
+        }
+        else
+        {
+            audioSource.Stop();
+        }
     }
 }
